71b1d37733a4f0e0b57543a087470d5de5f3fcce
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-11-30 16:34:08 +0100

    Initialize project using Create React App

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+};
