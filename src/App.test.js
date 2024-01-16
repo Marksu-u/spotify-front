c5369eb55075f8336d33cca2c0fd2a6ca9cbd070
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2024-01-16 12:11:36 +0100

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import Navbar from './components/Navbar';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component with router', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Navbar with search bar and profile info', () => {
+  render(<Navbar />);
+});
+
+// Vous pouvez ajouter des assertions ici pour vérifier si certains éléments sont présents
+// Par exemple, vérifiez si un élément spécifique de votre routeur est rendu
+// const navbarElement = screen.getByText(/texte spécifique dans le Navbar/);
+// expect(navbarElement).toBeInTheDocument();
